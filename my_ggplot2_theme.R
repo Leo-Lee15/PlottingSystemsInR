3a45538fcd8dd49be74d7c4be5e6613bf5a11a34
Guangzheng Li <lgz1993leo@163.com>
2017-08-21 15:28:48 +0800

    Update my_ggplot2_theme.R

--- my_ggplot2_theme.R
+++ my_ggplot2_theme.R
@@ -14,3 +14,32 @@ theme_plot2 <- theme(
   plot.title=element_text(size=20,face='bold'),
   strip.text.x = element_text(size=18,face='bold')
 )
+
+###-------------------------------------------------------------
+### Most used ggplot2 theme for plotting maps------------------
+###-------------------------------------------------------------
+windowsFonts(myFont = windowsFont("华文行楷"))  ## change the default font type to 华文行楷
+my_theme_map <- function(...) {
+  theme_minimal() +
+  theme(
+    text = element_text(family = "myFont", color = "#22211d"),
+    axis.line = element_blank(),
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    # panel.grid.minor = element_line(color = "#ebebe5", size = 0.2),
+    panel.grid.major = element_line(color = "#ebebe5", size = 0.2),
+    panel.grid.minor = element_blank(),
+    plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+    panel.background = element_rect(fill = "#f5f5f2", color = NA), 
+    legend.background = element_rect(fill = "#f5f5f2", color = NA),
+    panel.border = element_blank(),
+    ...
+  )
+}
+
+
+
+
